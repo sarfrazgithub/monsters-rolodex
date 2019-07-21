7b2843ca69f0546669054cdbb7f11acd9adf6e93
sarfrazgithub <sarfi.eee@gmail.com>
2019-07-21 16:04:01 +1000

    Updates

---
+++ precache-manifest.51aa33605797a2cdcbaff836a1966951.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afae4bb50bb6d0476b6b4d736ef4dfcc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "013d3d10ca1967f8c64a",
+    "url": "/monsters-rolodex/static/css/main.3875ec13.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "013d3d10ca1967f8c64a",
+    "url": "/monsters-rolodex/static/js/main.a8bc65e6.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
